406e7490543d542c1720dcb07ad87b84ad485546
Amin Adel <amin.adel@hotmail.com>
2019-04-10 16:22:56 +0430

    update 1398.01.21 - 16.20

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -6,6 +6,56 @@ import { Component, ViewEncapsulation } from '@angular/core';
 	styleUrls: ['./app.component.scss'],
 	encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-	title = 'angular';
+export class AppComponent implements OnInit {
+	
+	phrase = '';
+	results = [
+		{
+			id: 1,
+			phrase: 'some phrase',
+			type: 'name',
+			group: 'A1',
+			meaning: 'meaning',
+			examples: 'dasdads'
+		},
+		{
+			id: 2,
+			phrase: 'some phrase2',
+			type: 'name2',
+			group: 'A2',
+			meaning: 'meaning2',
+			examples: 'dasdads2'
+		}
+	];
+	form = 'create'; // create || edit
+	
+	// ==================================================
+	
+	constructor() {}
+	
+	ngOnInit(): void {
+		
+	}
+	
+	// ==================================================
+	
+	on_add_char(char) {
+		this.phrase += char;
+	}
+	
+	on_result_click(li_index) {
+		console.log(li_index);
+	}
+	
+	onSearch() {
+		console.log('search');
+	}
+	
+	onCreate() {
+		console.log('create');
+	}
+	
+	onEdit() {
+		console.log('edit');
+	}
 }
